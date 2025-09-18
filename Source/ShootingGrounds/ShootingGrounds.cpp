4b9b10283dd7b88484732474d2f04fa30d169e1d
cooler9190 <nikk.georgiev@abv.bg>
2025-09-18 09:34:41 +0200

    Deleting unnecessary files

--- Source/ShootingGrounds/ShootingGrounds.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ShootingGrounds.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShootingGrounds, "ShootingGrounds" );
-
-DEFINE_LOG_CATEGORY(LogShootingGrounds)
--- Source/ShootingGrounds/ShootingGroundsGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ShootingGroundsGameMode.h"
-
-AShootingGroundsGameMode::AShootingGroundsGameMode()
-{
-	// stub
-}
